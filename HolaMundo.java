def3203ae241b46acdcd7d367ff48eeadf36021a
rider <korn159@li.com>
2023-03-10 22:42:00 -0600

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author rider
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
+
+
